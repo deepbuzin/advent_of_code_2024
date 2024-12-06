64e46599affb6ae9eea6392c59c117acf7e3d4d9
deepbuzin <contactbuzin@gmail.com>
2024-12-07 00:21:58 +0700

    Upload day 4 part 2 in Python

--- day4.py
+++ day4.py
@@ -1,3 +1,6 @@
+import re
+
+
 with open("input_day4.txt") as f:
     input_str = f.read().strip()
 
@@ -49,28 +52,50 @@ for line in lines + reverse_lines:
 print(xmas_count)
 
 
-# pattern = "M(AS)"
-#
-#
-# for offset in range(-num_diag, num_diag):
-#     if offset >= 0:
-#         row, col = i, i + offset
-#         diag = [matrix[row][col] for i in range(min(rows, cols - offset))]
-#     else:
-#         row, col = i - offset, i
-#         diag = [matrix[row][col] for i in range(min(rows + offset, cols))]
-#
-#     lines.append("".join(diag))
-#
-#
-# for offset in range(-num_diag, num_diag):
-#     if offset >= 0:
-#         anti_diag = [
-#             matrix[i][cols - i - offset - 1] for i in range(min(rows, cols - offset))
-#         ]
-#     else:
-#         anti_diag = [
-#             matrix[i - offset][cols - i - 1] for i in range(min(rows + offset, cols))
-#         ]
-#
-#     lines.append("".join(anti_diag))
+pattern = "M(AS)"
+
+
+def get_match_indices(diag):
+    line = "".join(diag)
+    mas_indices = [match.start(1) for match in re.finditer(pattern, line)]
+    reverse_mas_indices = [
+        len(line) - 1 - match.start(1) for match in re.finditer(pattern, line[::-1])
+    ]
+    return mas_indices + reverse_mas_indices
+
+
+diag_mases = []
+
+for offset in range(-num_diag, num_diag):
+    if offset >= 0:
+        diag = [matrix[i][i + offset] for i in range(min(rows, cols - offset))]
+        mas_indices = get_match_indices(diag)
+        diag_mases.extend([(i, i + offset) for i in mas_indices])
+    else:
+        diag = [matrix[i - offset][i] for i in range(min(rows + offset, cols))]
+        mas_indices = get_match_indices(diag)
+        diag_mases.extend([(i - offset, i) for i in mas_indices])
+
+
+anti_diag_mases = []
+
+for offset in range(-num_diag, num_diag):
+    if offset >= 0:
+        anti_diag = [
+            matrix[i][cols - i - offset - 1] for i in range(min(rows, cols - offset))
+        ]
+        mas_indices = get_match_indices(anti_diag)
+        anti_diag_mases.extend([(i, cols - i - offset - 1) for i in mas_indices])
+    else:
+        anti_diag = [
+            matrix[i - offset][cols - i - 1] for i in range(min(rows + offset, cols))
+        ]
+        mas_indices = get_match_indices(anti_diag)
+        anti_diag_mases.extend([(i - offset, cols - i - 1) for i in mas_indices])
+
+x_mas_count = 0
+for pair in anti_diag_mases:
+    if pair in diag_mases:
+        x_mas_count += 1
+
+print(x_mas_count)
