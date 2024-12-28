2c4ebe5b6efda22f7a9dc2bb5c00e95c0d4f4174
deepbuzin <contactbuzin@gmail.com>
2024-12-29 02:00:26 +0700

    Add proper solution for day 12 with corners in Python

--- day12.py
+++ day12.py
@@ -2,18 +2,6 @@ from queue import Queue
 from typing import Tuple
 import numpy as np
 
-input_str = """
-RRRRIICCFF
-RRRRIICCCF
-VVRRRCCFFF
-VVRCCCJFFF
-VVVVCJJCFE
-VVIVCCJJEE
-VVIIICJJEE
-MIIIIIJJEE
-MIIISIJEEE
-MMMISSJEEE
-""".strip()
 
 with open("input_day12.txt", "r") as f:
     input_str = f.read().strip()
@@ -27,70 +15,90 @@ mask = np.zeros((h, w), dtype=bool)
 def find_plot(plant: str, q: Queue[Tuple[int, int]]) -> int:
     perimeter = 0
     area = 0
-    sides = 0
 
-    side_masks = {
-        "up": np.zeros((h + 2, w + 2), dtype=bool),
-        "down": np.zeros((h + 2, w + 2), dtype=bool),
-        "left": np.zeros((h + 2, w + 2), dtype=bool),
-        "right": np.zeros((h + 2, w + 2), dtype=bool),
-    }
-
-    def mark_side_mask(direction, i, j):
-        side_masks[direction][i + 1, j + 1] = True
+    plot_mask = np.zeros_like(mask, dtype=bool)
 
     while not q.empty():
         i, j = q.get()
         mask[i, j] = True
+        plot_mask[i, j] = True
         area += 1
 
-        if 0 <= i - 1 < h and garden[i - 1][j] == plant:
-            if not mask[i - 1, j] and (i - 1, j) not in q.queue:
-                q.put((i - 1, j))
-        else:
-            perimeter += 1
-            mark_side_mask("up" , i - 1, j)
+        for _i, _j in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+            if 0 <= _i < h and 0 <= _j < w and garden[_i][_j] == plant:
+                if not mask[_i, _j] and (_i, _j) not in q.queue:
+                    q.put((_i, _j))
+            else:
+                perimeter += 1
 
-            # is new horizontal side in up direction
-            if not side_masks["up"][i - 1 + 1, j + 1 - 1] and not side_masks["up"][i - 1 + 1, j + 1 + 1]:
-                sides += 1
+    # find corners
+    corner_mask = np.zeros_like(plot_mask, dtype=int)
 
+    for i in range(h):
+        for j in range(w):
+            # is outer upper left
+            if (
+                plot_mask[i, j]
+                and not (0 <= j - 1 < w and plot_mask[i, j - 1])
+                and not (0 <= i - 1 < h and plot_mask[i - 1, j])
+            ):
+                corner_mask[i, j] += 1
+            # is outer upper right
+            if (
+                plot_mask[i, j]
+                and not (0 <= j + 1 < w and plot_mask[i, j + 1])
+                and not (0 <= i - 1 < h and plot_mask[i - 1, j])
+            ):
+                corner_mask[i, j] += 1
+            # is outer lower left
+            if (
+                plot_mask[i, j]
+                and not (0 <= j - 1 < w and plot_mask[i, j - 1])
+                and not (0 <= i + 1 < h and plot_mask[i + 1, j])
+            ):
+                corner_mask[i, j] += 1
+            # is outer lower right
+            if (
+                plot_mask[i, j]
+                and not (0 <= j + 1 < w and plot_mask[i, j + 1])
+                and not (0 <= i + 1 < h and plot_mask[i + 1, j])
+            ):
+                corner_mask[i, j] += 1
 
-        if 0 <= i + 1 < h and garden[i + 1][j] == plant:
-            if not mask[i + 1, j] and (i + 1, j) not in q.queue:
-                q.put((i + 1, j))
-        else:
-            perimeter += 1
-            mark_side_mask("down" , i + 1, j)
+            # is inner upper left
+            if (
+                plot_mask[i, j]
+                and (0 <= j - 1 < w and plot_mask[i, j - 1])
+                and (0 <= i - 1 < h and plot_mask[i - 1, j])
+                and not (0 <= i - 1 < h and 0 <= j - 1 < w and plot_mask[i - 1, j - 1])
+            ):
+                corner_mask[i, j] += 1
+            # is inner upper right
+            if (
+                plot_mask[i, j]
+                and (0 <= j + 1 < w and plot_mask[i, j + 1])
+                and (0 <= i - 1 < h and plot_mask[i - 1, j])
+                and not (0 <= i - 1 < h and 0 <= j + 1 < w and plot_mask[i - 1, j + 1])
+            ):
+                corner_mask[i, j] += 1
+            # is inner lower left
+            if (
+                plot_mask[i, j]
+                and (0 <= j - 1 < w and plot_mask[i, j - 1])
+                and (0 <= i + 1 < h and plot_mask[i + 1, j])
+                and not (0 <= i + 1 < h and 0 <= j - 1 < w and plot_mask[i + 1, j - 1])
+            ):
+                corner_mask[i, j] += 1
+            # is inner lower right
+            if (
+                plot_mask[i, j]
+                and (0 <= j + 1 < w and plot_mask[i, j + 1])
+                and (0 <= i + 1 < h and plot_mask[i + 1, j])
+                and not (0 <= i + 1 < h and 0 <= j + 1 < w and plot_mask[i + 1, j + 1])
+            ):
+                corner_mask[i, j] += 1
 
-            # is new horizontal side in up direction
-            if not side_masks["down"][i + 1 + 1, j + 1 - 1] and not side_masks["down"][i + 1 + 1, j + 1 + 1]:
-                sides += 1
-
-
-        if 0 <= j - 1 < w and garden[i][j - 1] == plant:
-            if not mask[i, j - 1] and (i, j - 1) not in q.queue:
-                q.put((i, j - 1))
-        else:
-            perimeter += 1
-            mark_side_mask("left" , i, j - 1)
-
-            # is new horizontal side in up direction
-            if not side_masks["left"][i + 1 - 1, j - 1 + 1] and not side_masks["left"][i + 1 + 1, j - 1 + 1]:
-                sides += 1
-
-
-        if 0 <= j + 1 < w and garden[i][j + 1] == plant:
-            if not mask[i, j + 1] and (i, j + 1) not in q.queue:
-                q.put((i, j + 1))
-        else:
-            perimeter += 1
-            mark_side_mask("right" , i, j + 1)
-
-            # is new horizontal side in up direction
-            if not side_masks["right"][i + 1 - 1, j + 1 + 1] and not side_masks["right"][i + 1 + 1, j + 1 + 1]:
-                sides += 1
-
+    sides = np.sum(corner_mask)
     return area, perimeter, sides
 
 
@@ -105,12 +113,8 @@ for i in range(h):
             area, perimeter, sides = find_plot(plant=garden[i][j], q=q)
             price = area * perimeter
             bulk_price = area * sides
-            # print(f"{garden[i][j]}, {area}, {perimeter}, {price}")
-            # print(mask)
-            print(sides)
             total_price += price
             total_bulk_price += bulk_price
-
 
 print(total_price)
 print(total_bulk_price)
